21a21cb92adba18fd5f75fd649957e3830bc2272
andycodesstuff <andrybusa@gmail.com>
2022-05-29 14:56:05 +0100

    Render empty window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use bevy::prelude::*;
 
 fn main() {
-  App::new().run();
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "FlyByWire A32NX MCDU".to_string(),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }
