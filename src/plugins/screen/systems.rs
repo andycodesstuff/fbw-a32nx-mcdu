ed1de09efa5086dfd81fc76723edb91566a160f5
andycodesstuff <andrybusa@gmail.com>
2022-06-05 00:54:04 +0200

    Fix MCDU lines being rendered in reverse order

--- src/plugins/screen/systems.rs
+++ src/plugins/screen/systems.rs
@@ -17,7 +17,7 @@ pub fn setup(mut commands: Commands) {
         .spawn_bundle(NodeBundle {
             style: Style {
                 position_type: PositionType::Absolute,
-                flex_direction: FlexDirection::Column,
+                flex_direction: FlexDirection::ColumnReverse,
                 padding: Rect::all(Val::Percent(SCREEN_PADDING)),
                 size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
                 ..default()
