9b6002d32804f0909ffd59e16f91f169ec5ad7a5
andycodesstuff <andrybusa@gmail.com>
2022-07-13 12:30:10 +0200

    Add text alignment support

--- src/plugins/screen/systems_utils.rs
+++ src/plugins/screen/systems_utils.rs
@@ -1,4 +1,7 @@
-use crate::SCREEN_ROWS;
+use crate::{
+    plugins::server::{ParsedText, TextFormatter, TextSegment},
+    SCREEN_ROWS,
+};
 use bevy::prelude::*;
 
 const FONT_SIZE_PERCENT: f32 = 0.90;
@@ -9,4 +12,125 @@ pub(super) fn compute_font_size(window: &Window) -> f32 {
     let font_size = window_height / (SCREEN_ROWS as f32) * FONT_SIZE_PERCENT;
 
     font_size
+}
+
+#[derive(Clone, Copy)]
+pub(super) enum TextAlign {
+    Left,
+    Center,
+    Right,
+}
+
+/// Computes all the TextBundles that make up the given parsed text
+pub(super) fn compute_text_bundles(
+    parsed_text: &ParsedText,
+    default_alignment: TextAlign,
+    is_label_row: bool,
+    asset_server: &AssetServer,
+    window: &Window,
+) -> Vec<TextBundle> {
+    let mut text_bundles: Vec<TextBundle> = Vec::new();
+    let mut left_text_sections: Vec<TextSection> = Vec::new();
+    let mut center_text_sections: Vec<TextSection> = Vec::new();
+    let mut right_text_sections: Vec<TextSection> = Vec::new();
+
+    for TextSegment { formatters, value } in parsed_text {
+        let mut font_name = if is_label_row {
+            "HoneywellMCDUSmall.ttf"
+        } else {
+            "HoneywellMCDU.ttf"
+        };
+        let mut color = Color::rgb_u8(0xff, 0xff, 0xff);
+        let mut align = default_alignment;
+
+        for formatter in formatters {
+            // Extract which font to use
+            font_name = match formatter {
+                TextFormatter::FontBig => "HoneywellMCDU.ttf",
+                TextFormatter::FontSmall => "HoneywellMCDUSmall.ttf",
+                _ => font_name,
+            };
+
+            // Extract which color to use
+            color = match formatter {
+                TextFormatter::ColorAmber => Color::rgb_u8(0xff, 0x9a, 0x00),
+                TextFormatter::ColorCyan => Color::rgb_u8(0x00, 0xff, 0xff),
+                TextFormatter::ColorGreen => Color::rgb_u8(0x00, 0xff, 0x00),
+                TextFormatter::ColorInop => Color::rgb_u8(0x66, 0x66, 0x66),
+                TextFormatter::ColorMagenta => Color::rgb_u8(0xff, 0x94, 0xff),
+                TextFormatter::ColorRed => Color::rgb_u8(0xff, 0x00, 0x00),
+                TextFormatter::ColorWhite => Color::rgb_u8(0xff, 0xff, 0xff),
+                TextFormatter::ColorYellow => Color::rgb_u8(0xff, 0xff, 0x00),
+                _ => color,
+            };
+
+            // Extract which alignment to use
+            match formatter {
+                #[rustfmt::skip]
+                TextFormatter::AlignLeft => { align = TextAlign::Left; }
+                #[rustfmt::skip]
+                TextFormatter::AlignRight => { align = TextAlign::Right; }
+                _ => {}
+            };
+        }
+
+        // Group together text sections based on their alignment
+        let text_section = TextSection {
+            value: value.clone(),
+            style: TextStyle {
+                font: asset_server.load(font_name),
+                font_size: compute_font_size(window),
+                color,
+            },
+        };
+
+        match align {
+            TextAlign::Left => left_text_sections.push(text_section),
+            TextAlign::Center => center_text_sections.push(text_section),
+            TextAlign::Right => right_text_sections.push(text_section),
+        };
+    }
+
+    let create_text_bundle = |align, sections| -> TextBundle {
+        let left_pos = match align {
+            TextAlign::Left => Val::Px(0.0),
+            TextAlign::Center => Val::Auto,
+            TextAlign::Right => Val::Undefined,
+        };
+        let right_pos = match align {
+            TextAlign::Left => Val::Undefined,
+            TextAlign::Center => Val::Auto,
+            TextAlign::Right => Val::Px(0.0),
+        };
+
+        TextBundle {
+            style: Style {
+                position_type: PositionType::Absolute,
+                position: Rect {
+                    left: left_pos,
+                    right: right_pos,
+                    top: Val::Undefined,
+                    bottom: Val::Undefined,
+                },
+                ..default()
+            },
+            text: Text {
+                sections,
+                ..default()
+            },
+            ..default()
+        }
+    };
+
+    if !left_text_sections.is_empty() {
+        text_bundles.push(create_text_bundle(TextAlign::Left, left_text_sections));
+    }
+    if !center_text_sections.is_empty() {
+        text_bundles.push(create_text_bundle(TextAlign::Center, center_text_sections));
+    }
+    if !right_text_sections.is_empty() {
+        text_bundles.push(create_text_bundle(TextAlign::Right, right_text_sections));
+    }
+
+    text_bundles
 }
